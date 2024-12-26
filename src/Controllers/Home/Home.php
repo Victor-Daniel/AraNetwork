90ab35fd6d82f76fd98b754f2da5159dffe66ba2
Victor-Daniel <victordanielgts@outlook.com>
2024-12-26 02:11:41 -0300

    Iniciando projeto AraNetwork

---
+++ src/Controllers/Home/Home.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers\Home;
+
+class Home{
+
+    public function Teste(){
+        echo "OK";
+    }
+
+}
+
+
+?>
---
+++ src/Controllers/Login/Login.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers\Login;
+
+class Login{
+
+    
+}
+
+?>
