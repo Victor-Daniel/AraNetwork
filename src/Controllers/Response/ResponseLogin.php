f5327532170cf9efb24c5d8ae52c4efe144d21ec
Victor Daniel <victordanielgts@outlook.com>
2025-01-06 17:59:21 -0300

    Iniciando o html do login

--- src/Controllers/Response/ResponseLogin.php
+++ src/Controllers/Response/ResponseLogin.php
@@ -13,16 +13,23 @@ class ResponseLogin{
         $checker = new FileChecker();
         $reader = new FileReader();
         $Render = new FileRender();
-        if( $checker->FileVerify($Route)){
-             $Content = $reader->Reader($Route);
-             $Content = $Render->Render($Content,["CSSFILE"=>$_ENV["ENDERECO_LOCAL"].$_ENV["CSSLINK"].$Route.".css"]);
-             http_response_code(200);
-             return $Content;
+        if($Route=="/server-error"){
+            $Content = $reader->Reader("/server-error500");
+            http_response_code(500);
+            return $Content;
         }
         else{
-            $Content = $reader->Reader("/erro-page404");
-            http_response_code(404);
-            return $Content;
+            if( $checker->FileVerify($Route)){
+                $Content = $reader->Reader($Route);
+                $Content = $Render->Render($Content,["CSSFILEPC"=>$_ENV["ENDERECO_LOCAL"].$_ENV["CSSLINK"].$Route.".css"]);
+                http_response_code(200);
+                return $Content;
+           }
+           else{
+               $Content = $reader->Reader("/erro-page404");
+               http_response_code(404);
+               return $Content;
+           }
         }
         
     }
--- src/Utilities/FileChecker.php
+++ src/Utilities/FileChecker.php
@@ -3,6 +3,7 @@ namespace App\Utilities;
 
 class FileChecker{
 
+    // VERIFICADOR DE ARQUIVOS HTML
     public function FileVerify($filename){
         $default_path = __DIR__."../../Controllers/View".$filename.".html";
         if(file_exists($default_path)){
