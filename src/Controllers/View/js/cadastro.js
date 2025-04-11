15cf0c2efe0b94fd63c97801a003c2ff21a23cf3
Victor Daniel <victordanielgts@outlook.com>
2025-04-11 17:53:46 -0300

    Iniciando alteração no js do cadastro

--- src/Controllers/View/js/cadastro.js
+++ src/Controllers/View/js/cadastro.js
@@ -1,4 +1,15 @@
 var btnCadastrar = document.querySelector("#btn_Cadastrar");
+var cb_cpf = document.querySelector("#Pfisica");
+var cb_cnpj = document.querySelector("#Pjuridica");
+var Inp_RG = document.getElementById("RG");
+var Inp_CPF =  document.getElementById("CPF");
+var Inp_CNPJ =  document.getElementById("CNPJ");
+
 btnCadastrar.addEventListener("click",function e(){
-    alert("funcionando");
+    if(cb_cpf.checked){
+        alert("teste");
+    }
+    else if(cb_cnpj.checked){
+    }
 });
+
