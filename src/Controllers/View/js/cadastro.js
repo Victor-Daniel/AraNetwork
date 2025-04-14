ae7651951215b07e5d3ead61c8cca317b9d9c3ad
Victor Daniel <victordanielgts@outlook.com>
2025-04-14 17:44:13 -0300

    Iniciando as funções do Javascript da página cadastro.

--- src/Controllers/View/js/cadastro.js
+++ src/Controllers/View/js/cadastro.js
@@ -1,15 +1,25 @@
 var btnCadastrar = document.querySelector("#btn_Cadastrar");
-var cb_cpf = document.querySelector("#Pfisica");
-var cb_cnpj = document.querySelector("#Pjuridica");
-var Inp_RG = document.getElementById("RG");
-var Inp_CPF =  document.getElementById("CPF");
-var Inp_CNPJ =  document.getElementById("CNPJ");
+var rb_cpf = document.querySelector("#Pfisica");
+var rb_cnpj = document.querySelector("#Pjuridica");
+var inp_CNPJ =  document.getElementById("CNPJ");
+var lb_CNPJ = document.getElementById("lb_CNPJ");
+
+document.addEventListener("DOMContentLoaded",function e(){
+    rb_cpf.checked=true;
+    if(rb_cpf.checked==true){
+        inp_CNPJ.disabled=true;
+        inp_CNPJ.style.borderColor="#cececece";
+        lb_CNPJ.style.color="#cecece";   
+    }
+});
+
+rb_cpf.addEventListener("click",function e(){
+    inp_CNPJ.disabled=true;
+    inp_CNPJ.style.borderColor="#cececece";
+    lb_CNPJ.style.color="#cecece";
+});
 
 btnCadastrar.addEventListener("click",function e(){
-    if(cb_cpf.checked){
-        alert("teste");
-    }
-    else if(cb_cnpj.checked){
-    }
+
 });
 
